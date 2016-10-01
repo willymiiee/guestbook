a58fc40dde0f5b6b183e958855ad0319886670b4
Willy Nugraha <willymiiee@gmail.com>
2016-10-01 08:52:59 +0700

    REST API done

---
+++ app/Http/Controllers/GuestController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\Guest;
+
+class GuestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            $guests = Guest::all();
+        } catch (Exception $e) {
+            return response()->json([
+                'errors'    => $e->getMessage()
+            ]);
+        }
+
+        return response()->json([
+            'items' => $guests,
+            'count' => count($guests)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if (!request()->has('name')) {
+            return response()->json([
+                'errors'    => 'Nama harus diisi!'
+            ]);
+        }
+
+        $guest = new Guest;
+        $guest->name = request()->get('name');
+        $guest->save();
+
+        return response()->json([
+            'message'    => 'Data berhasil dimasukkan.'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            $guest = Guest::find($id);
+        } catch (Exception $e) {
+            return response()->json([
+                'errors'    => $e->getMessage()
+            ]);
+        }
+
+        return response()->json($guest);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if (!request()->has('name')) {
+            return response()->json([
+                'errors'    => 'Nama harus diisi!'
+            ]);
+        }
+
+        try {
+            $guest = Guest::find($id);
+        } catch (Exception $e) {
+            return response()->json([
+                'errors'    => $e->getMessage()
+            ]);
+        }
+
+        $guest->name = request()->get('name');
+        $guest->save();
+
+        return response()->json([
+            'message'    => 'Data berhasil diupdate.'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $guest = Guest::find($id);
+        } catch (Exception $e) {
+            return response()->json([
+                'errors'    => $e->getMessage()
+            ]);
+        }
+
+        $guest->delete();
+
+        return response()->json([
+            'message'    => 'Data berhasil dihapus.'
+        ]);
+    }
+}
